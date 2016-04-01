894f1344028b611e1add3617d38dc723e0b530b6
Neil Grogan <neil@ngrogan.com>
2016-04-01 11:19:20 +0100

    Added a manifest & main class so jar can be run
    - Manifest specifies the main class to be run
    - Added the main class as a runner, with "Hello, World!" as default for
      now

---
+++ src/main/java/com/ngrogan/core/Runner.java
@@ -0,0 +1,11 @@
+package com.ngrogan.core;
+
+public final class Runner {
+
+    private Runner(){}
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
